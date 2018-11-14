7f2f24d157f7cf78d879487876be652995bdf531
Finbar Ryan <finbarr@microsoft.com>
2018-11-14 15:24:04 +0200

    Add project files.

---
+++ AspNetCoreLinux/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreLinux.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
